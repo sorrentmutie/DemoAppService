7e8927946836d65dbda7882cfcf7efe8af3ee9b1
salvatore <salvatore.sorrentino@live.com>
2023-05-30 16:20:15 +0200

    Aggiungere i file di progetto.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoAppService.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        private readonly IConfiguration configuration;
+        public string? Saluto { get; set; }
+        public string? Saluto2 { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            Saluto = configuration["Saluto"];
+            Saluto2 = configuration["Saluto2"];
+           // throw new Exception("Errore gravissimo");
+        }
+    }
+}
